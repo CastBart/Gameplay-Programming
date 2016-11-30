da1c5b6a1c5987c5e29a7a68560f77c021a5684f
Bartosz Zych <C00205464@itcarlow.ie>
2016-11-30 22:48:47 +0000

    Lab 6 cube rotations

--- lab 6/Game.h
+++ lab 6/Game.h
@@ -3,9 +3,11 @@
 #include <SFML/OpenGL.hpp>
 #include <gl/GL.h>
 #include <gl/GLU.h>
+#include "Vector3.h"
+#include "Matrix3.h"
 
 using namespace std;
-using namespace sf;
+
 
 class Game
 {
@@ -14,17 +16,21 @@ public:
 	~Game();
 	void run();
 private:
-	Window window;
+	sf::Window window;
 	bool isRunning = false;
 	void initialize();
 	void update();
 	void draw();
 	void unload();
+	void rotatePoint(double angle, Matrix3::Axis axis);
+	void translatePoints();
+	void scalePoints();
 	const int primitives;
 
 	GLuint index;
-	Clock clock;
-	Time elapsed;
-
+	sf::Clock clock;
+	sf::Time elapsed;
+	Vector3 m_cubePoints[8];
+	Matrix3 rotationMatrix;
 	float rotationAngle = 0.0f;
 };
