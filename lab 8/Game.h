a559f44926f5a452899a8d28be8d22ffc181ab4a
(Student) - Bartosz Zych <C00205464@itcarlow.ie>
2016-12-15 13:55:16 +0000

    lab 8

---
+++ lab 8/Game.h
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <gl/glew.h>
+#include <gl/wglew.h>
+#include <SFML/Window.hpp>
+#include <SFML/OpenGL.hpp>
+#include <Vector3.h>
+#include <Matrix3.h>
+
+
+using namespace std;
+
+class Game
+{
+public:
+	Game();
+	~Game();
+	void run();
+private:
+	sf::Window window;
+	bool isRunning = false;
+	void initialize();
+	void transformCube(Matrix3 &);
+	void update();
+	void render();
+	void unload();
+	Vector3 m_cubePoints[8];
+	Matrix3 matrix;
+	sf::Clock clock;
+	sf::Time elapsed;
+
+	float rotationAngle = 0.0f;
+};
